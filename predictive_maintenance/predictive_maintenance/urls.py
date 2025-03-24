1cd64f370ca04bbe1d7cb5253b096eb6454c8eb7
Bang28 <mhdmasruri9@gmail.com>
2025-03-24 10:23:47 +0700

    third commit

--- predictive_maintenance/predictive_maintenance/urls.py
+++ predictive_maintenance/predictive_maintenance/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('monitoring.urls'))
+    path('apm-machine/',include('monitoring.urls'))
 ]
